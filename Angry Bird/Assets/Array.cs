02fce4d93e12f32f3c029be8a03b10730f7416a1
arnav003 <lalaarnav003@gmail.com>
2022-08-04 00:57:17 +0530

    Initial commit

---
+++ Angry Bird/Assets/Array.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+
+    int a = 5;
+
+    public int[] myArray = new int[5];
+
+    public int[] MyArray2 = { 12, 76, 8, 10 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = 5;
+
+       foreach (int i in MyArray2)
+        {
+            Debug.Log(i);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void newFunction(int a , int u, float f)
+    {
+        Debug.Log("Hello");
+    }
+
+    public void newFuntion ()
+    {
+        Debug.Log("Hi there");
+    }
+}
---
+++ Angry Bird/Assets/Bird/Bird.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    Vector3 _initialPosition;
+
+    private bool _birdWadLaunched = false;
+
+    private float _timeSittingAround;
+
+    [SerializeField]
+    private float _launchPower = 0;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 _currentPos = new Vector3(transform.position.x, transform.position.y);
+        GetComponent<LineRenderer>().SetPosition(0, _currentPos);
+
+        Vector3 _initialPos = new Vector3(_initialPosition.x, _initialPosition.y);
+        GetComponent<LineRenderer>().SetPosition(1, _initialPos);
+        
+
+        if (_birdWadLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 10 || transform.position.y < -10 || transform.position.x > 15 || transform.position.x < -15 || _timeSittingAround > 3)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToIntialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToIntialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+
+        _birdWadLaunched = true;
+
+        GetComponent<LineRenderer>().enabled = false;
+    }
+
+    private void OnMouseDrag()
+    {        
+        Vector3 newPosition =Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+    }
+
+}
